fe51f6a64dc8b8bfa534f6fc5c7997e438eb8c43
talamoig <ivano.talamo@gmail.com>
2015-09-14 10:57:21 +0200

    update

--- manifests/package.pp
+++ manifests/package.pp
@@ -1,8 +1,29 @@
 class dcache::package {
 
-  package{'dcache':
-    ensure => $dcache::version,
+  $dcachepkg="dcache-${dcache::version}.${dcache::release}.noarch"
+
+  exec{'download-rpm':
+    command => "wget --no-check-certificate -O /tmp/${dcachepkg}.rpm http://www.dcache.org/downloads/1.9/repo/2.10/${dcachepkg}.rpm",
+    path    => '/usr/bin',
+    creates => "/tmp/${dcachepkg}.rpm"
+  }
+
+  file{"/tmp/${dcachepkg}.rpm":
+    ensure  => present,
+    require => Exec['download-rpm']
+  }
+  
+  package{$dcachepkg:
+    ensure => "${dcache::version}.${dcache::release}",
     provider => 'rpm',
-    source => "https://www.dcache.org/downloads/1.9/repo/${dcache::version}/dcache-${dcache::version}.${dcache::release}.noarch.rpm",
+    source => "/tmp/${dcachepkg}.rpm",
+    require => File["/tmp/${dcachepkg}.rpm"]
+  }
+
+  service{'dcache-server':
+    ensure => running,
+    hasstatus => true,
+    enable  => true,
+    require => Package[$dcachepkg]
   }
 }
